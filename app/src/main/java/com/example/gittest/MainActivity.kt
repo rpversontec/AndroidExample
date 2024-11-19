24af0d9fe08b0b04593e9aebc21ae368c93575b7
rpverson01 <rpverson01@tecmilenio.mx>
2024-11-19 14:41:59 -0600

    Se cambio el nombre a Maria

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GitTestTheme {
-        Greeting("Pepe Aguilar")
+        Greeting("Maria Juana")
     }
 }
